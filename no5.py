9331ab682ad63a62267da8bf662780be1a1b5e93
Shabian Arsyl Yonanta <72251541+shabianay@users.noreply.github.com>
2022-03-30 23:40:01 +0700

    Add files via upload

---
+++ no5.py
@@ -0,0 +1,6 @@
+desimal = int(input('Masukkan Bilangan Desimal = '))
+
+biner = bin(desimal) .replace("0b","")
+oktal = oct(desimal) .replace("0o","")
+hexa = hex(desimal) .replace("0x","")
+print(biner,oktal,hexa)
