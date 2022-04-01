b56fbe3e94ec53bdbd44b7aa8d28db3220b610a8
Shabian Arsyl Yonanta <72251541+shabianay@users.noreply.github.com>
2022-04-01 07:40:32 +0700

    Update no5.py

--- no5.py
+++ no5.py
@@ -1,6 +1,38 @@
-desimal = int(input('Masukkan Bilangan Desimal = '))
+class Stack:
+    def __init__(self):
+        self.items = []
 
-biner = bin(desimal) .replace("0b","")
-oktal = oct(desimal) .replace("0o","")
-hexa = hex(desimal) .replace("0x","")
-print(biner,oktal,hexa)
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+
+stack = Stack()
+string = "5*(4-2)"
+for i in string:
+    if i == '(' or i == '[' or i == '{':
+        stack.push(i)
+    elif i == ')' or i == ']' or i == '}':
+        if stack.isEmpty():
+            print(f'{i} is not a valid infix expression')
+            break
+        else:
+            if i == ')' and stack.pop() == '(':
+                continue
+            elif i == ']' and stack.pop() == '[':
+                continue
+            elif i == '}' and stack.pop() == '{':
+                continue
+            else:
+                print(f'{i} is not a valid character')
+                break
+else:
+    if stack.isEmpty():
+        print(f'{string} is a correct expression')
+    else:
+        print(f'{string} is not a correct expression')
